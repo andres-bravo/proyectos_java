dc82ce7abb9fb77064f78b1e3d29f45bf7a8ce14
Andres <andres.bravo.sanz@gmail.com>
2018-01-10 22:38:26 +0100

    Second version StringBuilder ArrayList and toArray method

--- 20_ejemplo_arraylist/src/principal/PruebaLista.java
+++ 20_ejemplo_arraylist/src/principal/PruebaLista.java
@@ -19,13 +19,30 @@ public class PruebaLista {
 			System.out.println(i);
 		}
 		miarray[0]=miarray[0]+100;
-		//num no se ve modificado porque los Integer son Inmutables
-		System.out.println(num.get(0));
+		//num.get(0) no se ve modificado porque los Integer son Inmutables
+		//en el array para la posición 0 se crea otro objeto con el contenido 0+100
+		System.out.println("Posición 0 del ArrayList de Integer: " + num.get(0));
+		System.out.println("Posición 0 del Array de Integer: " + miarray[0]);
 
-		//Lo mismo con StringBuilder que es mutable como cualquier otro objeto
-		//Hacerlo
+		//Creamos ArrayList de StringBuilder
+		ArrayList<StringBuilder> sb = new ArrayList<>();
+		sb.add(new StringBuilder("shell"));
+		sb.add(new StringBuilder("java"));
+		sb.add(new StringBuilder("python"));
+		for (StringBuilder i:sb) {
+			System.out.println(i);
+		}
 		
-		
+		StringBuilder[] miarraySB = sb.toArray(new StringBuilder[0]);
+		for (StringBuilder i:miarraySB) {
+			System.out.println(i);
+		}
+		//Modifico el array
+		miarraySB[2].append("version");
+		/*Imprimo el ArrayList de StringBuilder posicion 2 para ver si se ha modificado
+		  Veremos que imprime "python version" ya que ha sido modificado al modificar el array
+		  Esto sucede porque StringBuilder es mutable*/
+		System.out.println(sb.get(2));
 	}
 
 }
