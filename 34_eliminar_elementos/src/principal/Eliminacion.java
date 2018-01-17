25c910aef096a762aa6b0e39659cc47b936aa80c
manana <manana@Core11-41>
2018-01-17 09:50:13 +0100

    Version1, Expresiones Lambda uso

---
+++ 34_eliminar_elementos/src/principal/Eliminacion.java
@@ -0,0 +1,30 @@
+package principal;
+
+import java.util.ArrayList;
+
+public class Eliminacion {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numeros = new ArrayList();
+		numeros.add(10);
+		numeros.add(8);
+		numeros.add(15);
+		numeros.add(7);
+		numeros.add(41);
+		numeros.add(11);
+		/*
+		for (Integer i:numeros) {
+			if (i<15) numeros.remove(i);//foreach es sólo de lectura entoces falla al eliminar un elemento.
+		}
+		*/
+		//Tengo un método que utilizando expresion lambda puedo hacer esto
+		//removeIf(Predicate<? super E> filter)
+		//numeros.removeIf((Integer n)->{return n<15;}); //Expresion larga
+		numeros.removeIf(n->n<15); //Expresion reducida
+		
+		for (Integer i:numeros) {
+			System.out.println(i);
+		}
+	}
+
+}
