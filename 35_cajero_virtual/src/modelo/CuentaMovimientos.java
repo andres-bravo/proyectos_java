ab658b9146cbb1976b4c2e6913e0067a9573bab9
Andres <andres.bravo.sanz@gmail.com>
2018-01-18 19:21:33 +0100

    Version 3 - Implementado capa presentacion y clase CuentaMovimientos

--- 35_cajero_virtual/src/modelo/CuentaMovimientos.java
+++ 35_cajero_virtual/src/modelo/CuentaMovimientos.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 import modelo.Movimiento;
 public class CuentaMovimientos extends Cuenta{
 	private ArrayList<Movimiento> movimientos = new ArrayList();
-	private int limite;
-	public CuentaMovimientos(double saldo, int limite) {
+	private double limite;
+	public CuentaMovimientos(double saldo, double limite) {
 		super(saldo);
 		this.limite = limite;
 	}
+	/*
 	public boolean nuevoMovimiento(String tipo, double cantidad) {
 		if (cantidad>this.limite) {
 			return false;
@@ -18,6 +19,32 @@ public class CuentaMovimientos extends Cuenta{
 			this.ingresar(cantidad);
 		}else this.extraer(cantidad);
 		return true;
+	}
+	*/
+	@Override
+	public void ingresar(double cant) {
+		// TODO Auto-generated method stub
+		super.ingresar(cant);
+		Movimiento mov = new Movimiento("Ingreso",cant);
+		movimientos.add(mov);
+	}
+	public double getLimite() {
+		return limite;
+	}
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+	@Override
+	public void extraer(double cant) {
+		// TODO Auto-generated method stub
+		super.extraer(cant);
+		Movimiento mov = new Movimiento("Extraccion",cant);
+		movimientos.add(mov);
+	}
+	@Override
+	public double getSaldo() {
+		// TODO Auto-generated method stub
+		return super.getSaldo();
 	}
 	public ArrayList<Movimiento> listaMovimientos(){
 		return movimientos;
--- 35_cajero_virtual/src/presentacion/Cajero.java
+++ 35_cajero_virtual/src/presentacion/Cajero.java
@@ -7,16 +7,16 @@ import modelo.CuentaMovimientos;
 
 /*
  * Al inicial el programa, se solicita al usuario el saldo inicial y el 
- * límite de la cuenta (máxima cantidad a extraer en un proceso)
+ * lï¿½mite de la cuenta (mï¿½xima cantidad a extraer en un proceso)
  * Con herencia a partir de Cuenta hacer todo esto.
- * Una vez solicitado límite de la cuenta motrar el menu:
+ * Una vez solicitado lï¿½mite de la cuenta motrar el menu:
  * 1. Ingresar
  * 2. Extraer
  * 3. Movimientos y saldo
  * 4. Salir
  * 
  * 1. Se solicita la cantidad a ingresar y se procede al ingreso
- * 2. Se solicita la cantidad a extraer, si es inferior o igual al límite
+ * 2. Se solicita la cantidad a extraer, si es inferior o igual al lï¿½mite
  * se realiza la extraccion. si no no se hace
  * 3. Muestra los movimientos realizados en la cuenta y el saldo final.
  * Cada movimiento se caracteriza por un tipo (ingreso o extraccion) y una cantidad.
@@ -25,61 +25,53 @@ public class Cajero {
 
 	public static void main(String[] args) {
 		int op;
-		double saldo;
-		int limite;
-		int cantidad;
+		double saldo,limite,cantidad;
 		boolean resultado;
 		ArrayList<Movimiento> mov = new ArrayList();
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Introduzca Saldo Incial");
+		System.out.println("Introduzca Saldo Inicial");
 		saldo = Double.parseDouble(sc.nextLine());
 	    System.out.println("Introduzca Limite en Cajero");
-	    limite = 
+	    limite =Double.parseDouble(sc.nextLine());
+	    CuentaMovimientos cuenta = new CuentaMovimientos(saldo,limite);
+	    System.out.println("Saldo:"+cuenta.getSaldo()+ " Limite: "+ cuenta.getLimite());
 		do{
+			System.out.println("---------------MENU DE OPCIONES-----------------");
 			System.out.println("1.- Ingresar");
 		    System.out.println("2.- Extraer");
 		    System.out.println("3.- Movimientos y Saldo");
 		    System.out.println("4.- Salir");
+		    System.out.println("------------------------------------------------");
 		    op=Integer.parseInt(sc.nextLine());
 		    switch(op){
 		    	case 1:
 		    		System.out.println("Cantidad a Ingresar:");
-		            cantidad=Integer.parseInt(sc.nextLine());
-		            
-		            
-		            
-		            break;
-		        case 2:
-		        	/*
-		        	System.out.println("Email:");
-		            email=sc.nextLine();
-		            Contacto con= gcontactos.buscarContacto(email);
-		            if(con!=null) {
-		            	System.out.println(con.getNombre()+","+con.getEmail()+","+con.getTelefono()); 
-		                }else {
-		                	System.out.println("Contacto no encontrado"); 
-		                }*/
-		            break;
-		        case 3:
-		        	/*
-		        	 System.out.println("Email:");
-		             email=sc.nextLine();
-		             if(gcontactos.eliminarContacto(email)) {
-		            	 System.out.println("Contacto eliminado!"); 
-		             }else {
-		            	 System.out.println("No existe ese email!"); 
-		             }*/
-		             break;
-		        case 4:
-		        	/*
-		        	Contacto[] todos=gcontactos.recuperarContactos();
-		        	for(Contacto s:todos) {
-		        		System.out.println(s.getNombre()+","+s.getEmail()+","+s.getTelefono());
-		            }*/
-		        	break;
-		        default:
-		            System.out.println("debes escribir una opción válida");
+		        cantidad=Double.parseDouble(sc.nextLine());		       
+		        	cuenta.ingresar(cantidad);
+		        	System.out.println("Saldo:"+cuenta.getSaldo()+ " Limite: "+ cuenta.getLimite());
+		        break;
+		    case 2:
+	    			System.out.println("Cantidad a Extraer:");
+		        cantidad=Double.parseDouble(sc.nextLine());
+		        if (cantidad<=cuenta.getLimite()) {
+		        		cuenta.extraer(cantidad);
+		        		System.out.println("Saldo:"+cuenta.getSaldo()+ " Limite: "+ cuenta.getLimite());
 		        }
+		        else
+		        		System.out.println("Ha superado el limite.");
+		        break;
+		    case 3:
+		    		mov = cuenta.listaMovimientos();
+		    		System.out.println("---------------LISTADO DE MOVIMIENTOS----------");
+		    		mov.forEach(n->System.out.println("Tipo: "+ n.getTipo()+" Cantidad: "+n.getCantidad()));
+		    		System.out.println("---------------SALDO---------------------------");
+		    		System.out.println("Saldo: "+ cuenta.getSaldo());
+		        break;
+		    case 4:
+		        	break;
+		    default:
+		        System.out.println("debes escribir una opcion valida");
+		    }
 		    }while(op!=4);
 	}
 
